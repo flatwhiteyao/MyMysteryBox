9812b0826d2bbcd5f2d79b2aea864c292797880b
flatwhiteyao <3357661949@qq.com>
2025-07-27 09:27:07 +0800

    feat: 添加登录注册功能的CI/CD工作流 - 包含代码质量检查、多环境测试、构建测试和安全扫描

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    'react/prop-types': 'off'
+  }
+}; 
